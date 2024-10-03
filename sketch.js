5b4ede4a0a4cc7fe63b7e936edb6fa2be0899c19
rafa25tm <162468998+rafa25tm@users.noreply.github.com>
2024-10-03 11:53:32 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,22 @@
+//suits
+//lucifer
+//cobra kai
+//stranger things
+//psych
+//Big Bang: A Teoria
+//la casa de papel
+//Ted Lasso
+//The Boys
+//Breaking Bad
+//The Last of Us
+//Game of Thrones
+
+
+
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(220);
+}
